fee005d332800ef61193f955c8e0e7ac16add0f8
simonh8 <simonh8@nycstudents.net>
2023-10-25 12:31:08 -0400

    starting

---
+++ script.js
@@ -0,0 +1,6 @@
+const tiles = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+const tileContainer = document.querySelector('.tile-container');
+
+tiles.forEach((tile) => {
+    
+});
