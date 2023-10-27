2fe78cf39408434af2a741785d672081f6b1324b
simonh8 <simonh8@nycstudents.net>
2023-10-27 12:30:54 -0400

    Added drag and drop, win con, & fixed css,

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const tiles = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+const tiles = document.querySelectorAll('.tile');
 const tileContainer = document.querySelector('.tile-container');
 
-tiles.forEach((tile) => {
-    
-});
+function allowDrop(ev){
+    ev.preventDefault();
+}
+function drag(ev){
+    ev.dataTransfer.setData("text",ev.target.id);
+}
+function drop(ev){
+    ev.preventDefault();
+    if (ev.target.hasChildNodes()){return;}
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data))
+    checkForWin();
+}
+
+function checkForWin(){
+    let lock = "123456789101112131415";
+    let key = "";
+    for (let i = 0; i < tiles.length; i++){
+        key += document.getElementById(tiles).innerHTML;
+    }
+    console.log(key);
+}
