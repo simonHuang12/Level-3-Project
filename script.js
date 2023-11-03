8454804a02422383a72ece409eb53bde5ac65daf
simonHuang12 <simonh8@nycstudents.net>
2023-11-02 21:07:19 -0400

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tiles, tileContainer, board, shuffle, swapArr;
+let tiles, tileContainer, shuffle, gameWon, tileLocked;
 
 function initialize() {
     tiles = document.querySelectorAll('.tile');
@@ -7,31 +7,15 @@ function initialize() {
         const row = parseInt(i / 4);
         const col = i % 4;
         tiles[i].addEventListener("click", () => swapTiles(row, col));
+        //creates a 2d array and adds event listeners to each of them
     }
-    board = new Array(4).fill().map(() => new Array(4));
+    //conditions to prevent movement  
     shuffle = false;
-    swapArr = [];
+    gameWon = false;
+    tileLocked = true;
 }
 
-function checkForWin() {
-    let lock = Array.from({ length: 15 }, (_, i) => i + 1 + '');
-    lock.push('');
-    const key = [];
-    tiles.forEach(t => {
-        key.push(t.textContent);
-    });
-    if (arrayEquals(key, lock)) {
-        alert("You win");
-    }
-}
-
-function arrayEquals(arr1, arr2) {
-    return Array.isArray(arr1) &&
-        Array.isArray(arr2) &&
-        arr1.length === arr2.length &&
-        arr1.every((val, index) => val === arr2[index]);
-}
-
+//finds the empty div in the 2d array so it can be swapped with
 function emptyDiv() {
     for (let i = 0; i < tiles.length; i++) {
         const x = parseInt(i / 4);
@@ -43,50 +27,84 @@ function emptyDiv() {
 }
 
 function swapTiles(row, col) {
-    let swap = document.getElementById(""+row+col).textContent;
-    let adjRowUp, adjRowDown, adjColLeft, adjColRight;
-    if (row - 1 < 0) {
-        adjRowUp = 0;
-    }else{
-        adjRowUp = row-1;
-    }   
-    if (row + 1 > 3) {
-        adjRowDown = 3;
-    }else{
-        adjRowDown = row+1;
+    //conditons to prevent movement
+    if (tileLocked){
+        alert("Please shuffle the board before playing");
+        return;
     }
-    if (col - 1 < 0) {
-        adjColLeft = 0;
-    }else{
-        adjColLeft = col-1
+    if (gameWon) {
+        return;
     }
-    if (col + 1 > 3) {
-        adjColRight = 0;
-    }else{
-        adjColRight = col+1;
+    const emptyId = emptyDiv();
+    const clickedId = `${row}${col}`;
+
+    // checks if the clicked tile is adjacent to the empty tile
+    if (areTilesAdjacent(clickedId, emptyId)) {
+        // swaps the content of the clicked tile and the empty tile
+        const clickedTile = document.getElementById(clickedId);
+        const emptyContent = "";
+
+        document.getElementById(emptyDiv()).innerHTML = clickedTile.textContent;
+        clickedTile.textContent = emptyContent;
+    }
+    if (!shuffle){
+        checkForWin();
     }   
-    let adjRowUpTile, adjRowDownTile, adjColLeftTile, adjColRightTile;
-    adjRowUpTile = document.getElementById(""+adjRowUp+col).textContent;
-    adjRowDownTile = document.getElementById(""+adjRowDown+col).textContent;
-    adjColLeftTile = document.getElementById(""+row+adjColLeft).textContent;
-    adjColRightTile = document.getElementById(""+row+adjColRight).textContent; 
-
-    //console.log(adjColRightTile);
-    console.log(adjRowDown);
-    console.log(adjRowDownTile);
-    //console.log(adjColLeftTile);
-    //console.log(adjColRightTile);
-
-
-    console.log(emptyDiv(row, col));
 }
 
+//used in swapTiles to help check if the adj tile is an empty one
+function areTilesAdjacent(tile1Id, tile2Id) {
+    const [row1, col1] = tile1Id.split('').map(Number);
+    const [row2, col2] = tile2Id.split('').map(Number);
+
+    return (
+        (Math.abs(row1 - row2) === 1 && col1 === col2) ||
+        (row1 === row2 && Math.abs(col1 - col2) === 1)
+    );
+}
+
+//shuffles the board 1000 times by using swapTiles 1000 times for random tiles
 function shuffleBoard() {
-    for (let i = 0; i < 10; i++) {
+    shuffle = true;
+    tileLocked = false;
+    for (let i = 0; i < 1000; i++) {
         const x = Math.floor(Math.random() * 4);
         const y = Math.floor(Math.random() * 4);
-        shuffle = true;
         swapTiles(x, y);
     }
+    tileLocked = gameWon ? false: true;
     shuffle = false;
+}
+
+
+//used in checkForWin()
+function arrayEquals(arr1, arr2) {
+    return Array.isArray(arr1) &&
+        Array.isArray(arr2) &&
+        arr1.length === arr2.length &&
+        arr1.every((val, index) => val === arr2[index]);
+}
+
+//gets the text content of every single element in the 2d array in order
+//then it matches it with a pre-generated string of the correct order 
+function checkForWin() {
+    let lock = Array.from({ length: 15 }, (_, i) => i + 1 + '');
+    lock.push('');
+    const key = [];
+    tiles.forEach(t => {
+        key.push(t.textContent);
+    });
+    if (arrayEquals(key, lock)) {
+        gameWon = true;
+        const winMessage = document.getElementById('win-message');
+        winMessage.style.display = 'block';
+        //prompts win message because alert sucks
+    }
+}
+
+//close button for that window
+function closeWinMessage() {
+    const winMessage = document.getElementById('win-message');
+    winMessage.style.display = 'none';
+    location.reload();
 }
